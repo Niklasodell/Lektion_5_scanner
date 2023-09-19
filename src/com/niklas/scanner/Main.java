013347a54b26ef2b997907bd9ffe010d2c8cc9f7
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-09-19 13:05:19 +0200

    Third commit scanner

--- src/com/niklas/scanner/Main.java
+++ src/com/niklas/scanner/Main.java
@@ -1,5 +1,6 @@
 package com.niklas.scanner;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
@@ -7,16 +8,41 @@ public class Main {
 
         // Denna aktiverar subklassen //
 
+        boolean isPlaying = true;
+
         do {
-            System.out.println("Did this rune once?");
-        }while (false);
+            int result = scannerNumber();
+
+            if (result > 1000) {
+                isPlaying = false;
+                break;
+            }
+
+                for (int i = 0; i < result; i++) {
+                    System.out.println(i);
+                    System.out.println("Looping...");
+                }
+
+
+        }while (isPlaying);
 
     }
 
-    public static void scanner() {
+
+    public static int scannerNumber() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please insert a number");
+        return scan.nextInt();
+    }
+
+
+    public static String scannerText() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Insert some sentence");
-        scan.nextLine();
+        String input = scan.nextLine();
+
+        return input;
     }
 }
