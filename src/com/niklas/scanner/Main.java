7f5b8773b153697a036c8ed208705af7e7f55378
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-09-19 11:32:09 +0200

    Second commit scanner

--- src/com/niklas/scanner/Main.java
+++ src/com/niklas/scanner/Main.java
@@ -1,7 +1,22 @@
 package com.niklas.scanner;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        // Denna aktiverar subklassen //
+
+        do {
+            System.out.println("Did this rune once?");
+        }while (false);
+
+    }
+
+    public static void scanner() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Insert some sentence");
+        scan.nextLine();
     }
 }
