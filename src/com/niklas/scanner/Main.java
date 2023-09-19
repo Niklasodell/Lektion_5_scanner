1d1ac864e8241b67300c87ae7da167927a02723f
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-09-19 13:41:46 +0200

    Student Scanner implementation

--- src/com/niklas/scanner/Main.java
+++ src/com/niklas/scanner/Main.java
@@ -6,14 +6,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        // Objects
         Scanner scan = new Scanner(System.in);
+        Student benny = new Student();
 
+        // Game
         do {
 
-            System.out.println("Input Number!");
+            System.out.println("Input Benny's age!");
             int result = scan.nextInt();
 
+            benny.age = result;
+
             System.out.println("You picked: " + result);
+            System.out.println("Benny's age is: " + benny.age);
 
         } while (true);
 
---
+++ src/com/niklas/scanner/Student.java
@@ -0,0 +1,9 @@
+package com.niklas.scanner;
+
+public class Student {
+
+    public String name;
+    public int age;
+    public boolean isTired;
+
+}
