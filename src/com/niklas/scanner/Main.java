0f93a77db663781e4a99d862211f0ef349de0d0d
Niklasodell <144327353+Niklasodell@users.noreply.github.com>
2023-09-19 13:31:09 +0200

    Basic Scanner

--- src/com/niklas/scanner/Main.java
+++ src/com/niklas/scanner/Main.java
@@ -6,43 +6,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // Denna aktiverar subklassen //
-
-        boolean isPlaying = true;
+        Scanner scan = new Scanner(System.in);
 
         do {
-            int result = scannerNumber();
 
-            if (result > 1000) {
-                isPlaying = false;
-                break;
-            }
+            System.out.println("Input Number!");
+            int result = scan.nextInt();
 
-                for (int i = 0; i < result; i++) {
-                    System.out.println(i);
-                    System.out.println("Looping...");
-                }
+            System.out.println("You picked: " + result);
+
+        } while (true);
 
 
-        }while (isPlaying);
-
-    }
-
-
-    public static int scannerNumber() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Please insert a number");
-        return scan.nextInt();
-    }
-
-
-    public static String scannerText() {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.println("Insert some sentence");
-        String input = scan.nextLine();
-
-        return input;
     }
 }
